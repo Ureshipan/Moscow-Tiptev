e898c535d4a3e1d70f42353a13dde3b9d1fb64c8
Ureshipan <tiptevlad@yandex.ru>
2020-12-03 18:03:21 +0300

    push3

--- venv/first.py
+++ venv/first.py
@@ -1,1 +1,37 @@
-print('My first git-repo ')
+import pygame
+import sys
+
+
+def main():
+    pygame.init()
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+    screen.fill((0, 0, 0))
+    clock = pygame.time.Clock()
+    fps = 60
+    ex = False
+    ra = 100
+    rx = 0
+    ry = 0
+    rect_color = pygame.Color('green')
+    pygame.draw.rect(screen, rect_color, (rx, ry, ra, ra))
+
+    running = True
+    while running:
+        # внутри игрового цикла ещё один цикл
+        # приема и обработки сообщений
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = event.pos
+                if (x < rx or x > rx + ra or y < ry or y > ry + ra):
+
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
